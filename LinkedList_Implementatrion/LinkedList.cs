01f638be636d7566bc33868d27d7437dc9e59847
Shashikumar <shashirgowdas67@gmail.com>
2022-02-24 20:49:53 +0530

    Added:Create simple linked list

---
+++ LinkedList_Implementatrion/LinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Implimentation
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the list", node.data);
+        }
+    }
+}
---
+++ LinkedList_Implementatrion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList_Implimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wlcome to LinkedList program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
